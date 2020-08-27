8aaf6fed5e888c74e5a38a0b79734771ff60e450
Luke Becker <luke.becker@my.simpson.edu>
2020-08-27 14:26:23 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
-hi
+print("The Cardinals will make the playoffs this year.")
+print("I'm excited to be back on campus")
+print("I feel smart.")
+print("I don't know what else to say.")
+"\tI'm excited to learn programming."
