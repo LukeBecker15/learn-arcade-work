42de0c257e91c051083cbdd1e2fedd402838bceb
Luke Becker <luke.becker@my.simpson.edu>
2020-10-11 21:05:13 -0500

    Work on lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,111 +1,85 @@
+class Room:
+
+    def __init__(self, description, north, south, east, west):
+
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
+
 def main():
-
-
-
     room_list = []
-    a = "You are in the entrance to the Clue house\nThe path leads north"
-    n = 2
-    e = None
-    w = None
-    s = None
-    room_0 = [a, n, e, w, s]
-    room_list.append(room_0)
 
-    a1 = "You are in the billiard room\nThe path leads north and east"
-    n1 = 4
-    e1 = 2
-    w1 = None
-    s1 = None
-    room_1 = [a1, n1, e1, w1, s1]
-    room_list.append(room_1)
+    room = Room("You are in the entrance to the Clue house.\nThe path leads north.", 2, None, None, None)
+    room_list.append(room)
 
-    a2 = "You are in the ballroom\nThe path leads north, south, east, and west"
-    n2 = 5
-    e2 = 3
-    w2 = 1
-    s2 = 0
-    room_2 = [a2, n2, e2, w2, s2,]
-    room_list.append(room_2)
+    room = Room("You are in the billiard room.\nThe path leads north and east.", 4, None, 2, None)
+    room_list.append(room)
 
-    a3 = "You are in the kitchen\nThe path leads north and west"
-    n3 = 6
-    e3 = None
-    w3 = 2
-    s3 = None
-    room_3 = [a3, n3, e3, w3, s3]
-    room_list.append(room_3)
+    room = Room("You are in the ballroom.\nThe path leads north, south, east, and west.", 5, 0, 3, 1)
+    room_list.append(room)
 
-    a4 = "You are in the library\nThe path leads south and east"
-    n4 = None
-    e4 = 5
-    w4 = None
-    s4 = 1
-    room_4 = [a4, n4, e4, w4, s4]
-    room_list.append(room_4)
+    room = Room("You are in the kitchen.\nThe path leads north and west.", 6, None, None, 2)
+    room_list.append(room)
 
-    a5 = "You are in the study\nThe path leads south, east, and west"
-    n5 = None
-    e5 = 6
-    w5 = 4
-    s5 = 2
-    room_5 = [a5, n5, e5, w5, s5]
-    room_list.append(room_5)
+    room = Room("You are in the library.\nThe path leads south and east.", None, 1, 5, None)
+    room_list.append(room)
 
-    a6 = "You are in the dining room\nThe path leads south and west"
-    n6 = None
-    e6 = None
-    w6 = 5
-    s6 = 3
-    room_6 = [a6, n6, e6, w6, s6]
-    room_list.append(room_6)
+    room = Room("You are in the study.\nThe path leads south, east, and west.", None, 2, 6, 4)
+    room_list.append(room)
+
+    room = Room("You are in the dining room.\nThe path leads south and west.", None, 3, None, 5)
+    room_list.append(room)
+
 
     current_room = 0
 
-
     done = False
-    while not done:
-
+    while done == False:
         print()
-        print(room_list[current_room][0])
+        print(room_list[current_room].description)
 
-        move_room = input("What direction do you want to move? ")
-        if move_room.title() == "N" or move_room.title() == "North":
-            next_room = room_list[current_room][1]
-
-            if next_room == None:
-                print()
-                print("You can't go this way")
-            else:
-                current_room = next_room
-        elif move_room.title() == "S" or move_room.title() == "South":
-            next_room = room_list[current_room][4]
-            if next_room == None:
-                print()
-                print("You can't go this way")
-            else:
-                current_room = next_room
-
-        elif move_room.title() == "E" or move_room.title() == "East":
-            next_room = room_list[current_room][2]
+        next_room = input("What direction do you want to move? ")
+        if next_room.title() == "N" or next_room.title() == "North":
+            next_room = room_list[current_room].north
 
             if next_room == None:
                 print()
-                print("You can't go this way")
+                print("You can't go this way.")
+            else:
+                current_room = next_room
+        elif next_room.title() == "S" or next_room.title() == "South":
+            next_room = room_list[current_room].south
+            if next_room == None:
+                print()
+                print("You can't go this way.")
             else:
                 current_room = next_room
 
-        elif move_room.title() == "W" or move_room.title() == "West":
-            next_room = room_list[current_room][3]
+        elif next_room.title() == "E" or next_room.title() == "East":
+            next_room = room_list[current_room].east
 
             if next_room == None:
                 print()
-                print("You can't go this way")
+                print("You can't go this way.")
             else:
                 current_room = next_room
 
-        elif move_room.title() == "Q" or move_room.title() == "Quit":
+        elif next_room.title() == "W" or next_room.title() == "West":
+            next_room = room_list[current_room].west
+
+            if next_room == None:
+                print()
+                print("You can't go this way.")
+            else:
+                current_room = next_room
+
+        elif next_room.title() == "Q" or next_room.title() == "Quit":
             done = True
             print()
-            print("The game is over")
+            print("The game is over.")
+
 
 main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,208 +1,85 @@
-import random
-import arcade
-import math
+class Room:
 
-SPRITE_SCALING = 0.5
+    def __init__(self, description, north, south, east, west):
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-COIN_COUNT = 50
-ROCK_COUNT = 30
-
-class Coin(arcade.Sprite):
-
-    def __init__(self, filename, sprite_scaling):
-        """ Constructor. """
-        # Call the parent class (Sprite) constructor
-        super().__init__(filename, sprite_scaling)
-
-        # Current angle in radians
-        self.circle_angle = 0
-
-        # How far away from the center to orbit, in pixels
-        self.circle_radius = 0
-
-        # How fast to orbit, in radians per frame
-        self.circle_speed = 0.008
-
-        # Set the center of the point we will orbit around
-        self.circle_center_x = 0
-        self.circle_center_y = 0
-
-    def update(self):
-
-        """ Update the ball's position. """
-        # Calculate a new x, y
-        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
-                        + self.circle_center_x
-        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
-                        + self.circle_center_y
-
-        # Increase the angle in prep for the next round.
-        self.circle_angle += self.circle_speed + .02
-
-class Rock(arcade.Sprite):
-
-    def __init__(self, filename, sprite_scaling):
-        """ Constructor. """
-        # Call the parent class (Sprite) constructor
-        super().__init__(filename, sprite_scaling)
-
-        # Current angle in radians
-        self.circle_angle = 0
-
-        # How far away from the center to orbit, in pixels
-        self.circle_radius = 0
-
-        # How fast to orbit, in radians per frame
-        self.circle_speed = 0.008
-
-        # Set the center of the point we will orbit around
-        self.circle_center_x = 0
-        self.circle_center_y = 0
-
-    def update(self):
-
-        """ Update the ball's position. """
-        # Calculate a new x, y
-        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
-                        + self.circle_center_x
-        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
-                        + self.circle_center_y
-
-        # Increase the angle in prep for the next round.
-        self.circle_angle += self.circle_speed
-
-
-class MyGame(arcade.Window):
-
-    """ Main application class. """
-
-    def __init__(self, width, height):
-
-        super().__init__(width, height)
-
-        # Sprite lists
-        self.player_list = None
-        self.coin_list = None
-        self.rock_list = None
-
-        # Set up the player
-        self.score = 0
-        self.player_sprite = None
-
-        self.lives = 3
-        self.rock_sprite = None
-
-    def start_new_game(self):
-        """ Set up the game and initialize the variables. """
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-        self.rock_list = arcade.SpriteList()
-
-        # Set up the player
-        self.score = 0
-
-        # Character image from http://clipart-library.com/
-        self.player_sprite = arcade.Sprite("unnamed.png", SPRITE_SCALING / 3)
-
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 70
-        self.player_list.append(self.player_sprite)
-
-        for i in range(COIN_COUNT):
-
-
-            # Coin image from https://www.pinterest.com/
-            coin = Coin("c8884d6baa63c7497d28a9fae4f87a03.png", SPRITE_SCALING / 10)
-
-            # Position the center of the circle the coin will orbit
-            coin.circle_center_x = random.randrange(SCREEN_WIDTH)
-            coin.circle_center_y = random.randrange(SCREEN_HEIGHT)
-
-            # Random radius from 10 to 200
-            coin.circle_radius = random.randrange(10, 200)
-
-            # Random start angle from 0 to 2pi
-            coin.circle_angle = random.random() * 2 * math.pi
-
-            # Add the coin to the lists
-            self.coin_list.append(coin)
-
-
-        for i in range(ROCK_COUNT):
-
-
-            # Coin image from https://www.pinterest.com/
-            rock = Rock("images.png", SPRITE_SCALING / 2)
-
-            # Position the center of the circle the coin will orbit
-            rock.circle_center_x = random.randrange(SCREEN_WIDTH)
-            rock.circle_center_y = random.randrange(SCREEN_HEIGHT)
-
-            # Random radius from 10 to 200
-            rock.circle_radius = random.randrange(10, 200)
-
-            # Random start angle from 0 to 2pi
-            rock.circle_angle = random.random() * 2 * math.pi
-
-            # Add the coin to the lists
-            self.rock_list.append(rock)
-
-        # Don't show the mouse cursor
-        self.set_mouse_visible(False)
-
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def on_draw(self):
-
-        # This command has to happen before we start drawing
-        arcade.start_render()
-
-        # Draw all the sprites.
-        self.coin_list.draw()
-        self.player_list.draw()
-        self.rock_list.draw()
-
-        # Put the text on the screen.
-        output = "Score: " + str(self.score)
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
-
-    def update(self, delta_time):
-        """ Movement and game logic """
-
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        self.coin_list.update()
-        self.rock_list.update()
-
-        # Generate a list of all sprites that collided with the player.
-        good_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                        self.coin_list)
-        bad_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                        self.rock_list)
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for coin in good_hit_list:
-            self.score += 1
-            coin.remove_from_sprite_lists()
-
-        for rock in bad_hit_list:
-            self.lives -= 1
-            rock.remove_from_sprite_lists()
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
 
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    window.start_new_game()
-    arcade.run()
+    room_list = []
+
+    room = Room("You are in the entrance to the Clue house.\nThe path leads north.", 2, None, None, None)
+    room_list.append(room)
+
+    room = Room("You are in the billiard room.\nThe path leads north and east.", 4, None, 2, None)
+    room_list.append(room)
+
+    room = Room("You are in the ballroom.\nThe path leads north, south, east, and west.", 5, 0, 3, 1)
+    room_list.append(room)
+
+    room = Room("You are in the kitchen.\nThe path leads north and west.", 6, None, None, 2)
+    room_list.append(room)
+
+    room = Room("You are in the library.\nThe path leads south and east.", None, 1, 5, None)
+    room_list.append(room)
+
+    room = Room("You are in the study.\nThe path leads south, east, and west.", None, 2, 6, 4)
+    room_list.append(room)
+
+    room = Room("You are in the dining room.\nThe path leads south and west.", None, 3, None, 5)
+    room_list.append(room)
 
 
-if __name__ == "__main__":
-    main()
+    current_room = 0
+
+    done = False
+    while done == False:
+        print()
+        print(room_list[current_room].description)
+
+        next_room = input("What direction do you want to move? ")
+        if next_room.title() == "N" or next_room.title() == "North":
+            next_room = room_list[current_room].north
+
+            if next_room == None:
+                print()
+                print("You can't go this way")
+            else:
+                current_room = next_room
+        elif next_room.title() == "S" or next_room.title() == "South":
+            next_room = room_list[current_room].south
+            if next_room == None:
+                print()
+                print("You can't go this way")
+            else:
+                current_room = next_room
+
+        elif next_room.title() == "E" or next_room.title() == "East":
+            next_room = room_list[current_room].east
+
+            if next_room == None:
+                print()
+                print("You can't go this way")
+            else:
+                current_room = next_room
+
+        elif next_room.title() == "W" or next_room.title() == "West":
+            next_room = room_list[current_room].west
+
+            if next_room == None:
+                print()
+                print("You can't go this way")
+            else:
+                current_room = next_room
+
+        elif next_room.title() == "Q" or next_room.title() == "Quit":
+            done = True
+            print()
+            print("The game is over")
+
+
+main()
