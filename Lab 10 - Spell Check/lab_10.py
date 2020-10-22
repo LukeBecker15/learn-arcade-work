6b834469cda089223721f6f536cb293a6597a89c
Luke Becker <luke.becker@my.simpson.edu>
2020-10-22 14:04:06 -0500

    Work on lab 10

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -5,37 +5,66 @@ def split_line(line):
 
 def main():
 
-    my_file = open("AliceInWonderLand200.txt")
+    dictionary_file = open("dictionary.txt")
+    dictionary_list = []
 
-    dictionary_list = ["dictionary.txt"]
+    for line in dictionary_file:
+        line = line.strip()
+        dictionary_list.append(line)
+
+    dictionary_file.close()
 
 
     print("--Linear Search--")
 
-    for line in my_file:
+    my_file = open("AliceInWonderLand200.txt")
 
-        split_line(line)
+    line_number = 0
+
+    for line in my_file:
+        line_number += 1
+        line = line.strip()
         word_list = split_line(line)
-        for word in my_file:
+
+        for word in word_list:
+            word = word.strip()
+
             key = word.upper()
             current_position = 0
-            while current_position < len(dictionary_list) and dictionary_list[current_position] == key:
+            while current_position < len(dictionary_list) and dictionary_list[current_position] != key:
                 current_position += 1
-            if dictionary_list != key:
-                print("possible misspelled word:", key)
+
+            if current_position == len(dictionary_list):
+
+                print("Line", line_number, "possible misspelled word:", key)
+
+
+    my_file.close()
+
+
+
+
 
             # Create variable for line number
 
     print("--Binary Search--")
-    for line in my_file:
+    my_file = open("AliceInWonderLand200.txt")
+    line_number = 0
 
-        split_line(line)
+    for line in my_file:
+        line_number += 1
+        line = line.strip()
         word_list = split_line(line)
-        for word in my_file:
+
+        for word in word_list:
+            word = word.strip()
+
+
             key = word.upper()
             lower_bound = 0
             upper_bound = len(dictionary_list) - 1
             found = False
+
 
             while lower_bound <= upper_bound and not found:
                 middle_pos = (lower_bound + upper_bound) // 2
@@ -44,11 +73,13 @@ def main():
                     lower_bound = middle_pos + 1
                 elif dictionary_list[middle_pos] > key:
                     upper_bound = middle_pos - 1
-                else:
+                elif dictionary_list[middle_pos] == key:
                     found = True
 
-                if found:
-                    print(middle_pos)
+            if not found:
+                print("Line", line_number, "possible misspelled word:", key)
+
+    my_file.close()
 
 
 main()
